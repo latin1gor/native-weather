2ef04342d2dd33c019067d24d6dca7172008f3e0
latinigor <latinigor.main@gmail.com>
2024-08-07 16:47:47 +0300

    new commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,57 @@
+import {Image, View, Text, SafeAreaView, TextInput, TouchableOpacity} from 'react-native';
+
+import {StatusBar} from "expo-status-bar";
+import {MagnifyingGlassIcon} from "react-native-heroicons/outline";
+import {useState} from "react";
+import {MapPinIcon} from "react-native-heroicons/mini";
+
+
+export default function HomeScreen() {
+    const [showSearch, setShowSearch] = useState(false)
+    const [locations] = useState([1, 2, 3])
+
+
+
+    return (
+        <View className="flex-1 relative">
+            <StatusBar style={"light"}/>
+            <Image blurRadius={10} source={require("../../assets/images/main-bg.jpg")}
+                   className="absolute h-full w-full "/>
+
+
+            <SafeAreaView className="flex flex-1">
+                <View style={{height: "7%"}} className={"mx-4 relative z-50"}>
+                    <View
+                        className={`flex-row justify-end items-center rounded-full ${showSearch && "bg-gray-400/20"}`}>
+                        {
+                            showSearch ? (
+                                <TextInput placeholder={"Search city"} placeholderTextColor={"lightgray"}
+                                           className={"text-white text-base flex-1 pl-6 h-10"}/>
+
+                            ) : null
+                        }
+                        <TouchableOpacity className={"rounded-full bg-white/30 p-3 m-1"}
+                                          onPress={() => setShowSearch(prev => !prev)}><MagnifyingGlassIcon size={"25"}
+                                                                                                            color={"white"}/></TouchableOpacity>
+                    </View>
+                    {(locations.length > 0 && showSearch) ? (
+                        <View className={"absolute w-full bg-gray-300 top-16 rounded-3xl"}>
+                            {locations.map((l, i) => {
+                            const showBorder = i+1 != locations.length
+                                const borderClass = showBorder ? " border-b-2 border-b-gray-400" : ""
+                                return (
+                                <TouchableOpacity key={i}
+                                                  className={`flex-row items-center border-0 p-3 px-4 mb-1 ${borderClass}`}>
+                                    <MapPinIcon color={"gray"}/>
+                                    <Text className={"text-black text-lg ml-2"}>London, United Kingdom</Text>
+                                </TouchableOpacity>
+                                )
+
+                            })}
+                        </View>
+                    ) : null}
+                </View>
+            </SafeAreaView>
+        </View>
+    );
+}
